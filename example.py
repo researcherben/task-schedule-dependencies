c7d236c05ed214f950a7247894492b68cdf8f494
Ben Payne <ben.is.located@gmail.com>
2023-09-02 10:54:44 -0400

    works; done?

--- example.py
+++ example.py
@@ -4,7 +4,7 @@
 # https://pygraphviz.github.io/documentation/stable/
 import pygraphviz
 import random
-
+import copy # https://docs.python.org/3/library/copy.html
 """
 * Universal task ID. Randomly generated and uniquely assigned.
 * instance task id. Randomly generated and uniquely assigned. Enables duplication of task
@@ -63,6 +63,7 @@ class Task:
         self.cumulative_cost_and_duration = []
         self.output_parameters_dict = output_parameters_dict
         self.followed_by_task_instance_IDs = followed_by_task_instance_IDs
+        self.child_tasks = []
 
     def print(self):
         #print('persistent ID =', self.persistent_ID)
@@ -73,44 +74,59 @@ class Task:
         print('output_parameters_dict =', self.output_parameters_dict)
         print('followed_by_task_instance_IDs =', self.followed_by_task_instance_IDs)
 
+
+
+def which_task_is_instance_ID(instance_ID, all_tasks):
+    """
+    """
+    print("[TRACE] which_task_is_instance_ID: ",instance_ID)
+    #print("looking for ",instance_ID)
+    #print(type(instance_ID))
+    #print("looking in ",all_tasks)
+    for key_tuple, task in all_tasks.items():
+        task_description = key_tuple[0]
+        this_instance_ID = key_tuple[1]
+        #print("does ",instance_ID,"match",this_instance_ID)
+        #print(type(this_instance_ID))
+        if this_instance_ID==instance_ID:
+            return all_tasks[task_description,this_instance_ID]
+    raise Exception("instance_ID ",instance_ID,"not found in all_tasks")
+    return
+
 def cumulative_probability_of_task_success(task, all_tasks):
     """
     """
     print("[TRACE] cumulative_probability_of_task_success: ",task.description)
     return all_tasks
 
-def sum_cost_and_duration(task, all_tasks):
+def cost_and_duration_sum(task, all_tasks):
     """
     """
-    print("[TRACE] sum_cost_and_duration: ",task.description)
-    # what is the prior task?
-    cumulative = []
-    for candidate_prior_task_description, candidate_prior_task in all_tasks.items():
-        for instance_ID in candidate_prior_task.followed_by_task_instance_IDs:
-            if task.instance_ID == instance_ID:
-                print("   sum_cost_and_duration: ", candidate_prior_task.description," is followed by",task.description)
-                if len(candidate_prior_task.cumulative_cost_and_duration)==0:
-                    print("   sum_cost_and_duration: cannot increment empty list")
-                cumulative = []
-                for index,old_cost_dur_tuple in enumerate(candidate_prior_task.cumulative_cost_and_duration):
-                    cumulative.append((old_cost_dur_tuple[0]+task.cost_duration_tuples_list[index][0],
-                                       old_cost_dur_tuple[1]+task.cost_duration_tuples_list[index][1]))
-    return cumulative, all_tasks
+    print("[TRACE] cost_and_duration_sum: ",task.description)
 
-def add_task(G, task, all_tasks):
-    """
-    """
-    print("[TRACE] add_task: ",task.description)
-    if task.description=="start here":
-        all_tasks[(task.description, task.instance_ID)].cumulative_cost_and_duration = task.cost_duration_tuples_list
+    for follow_on_instance_ID in task.followed_by_task_instance_IDs:
+        follow_on_task = which_task_is_instance_ID(follow_on_instance_ID, all_tasks)
+        #all_tasks[follow_on_task.description,follow_on_task.instance_ID].cumulative_cost_and_duration = []
+        for index, cost_dur_tuple in enumerate(task.cumulative_cost_and_duration):
+            all_tasks[follow_on_task.description,follow_on_task.instance_ID].cumulative_cost_and_duration.append(
+                (cost_dur_tuple[0]+follow_on_task.cost_duration_tuples_list[index][0],
+                 cost_dur_tuple[1]+follow_on_task.cost_duration_tuples_list[index][1])
+            )
+        all_tasks[follow_on_task.description,follow_on_task.instance_ID].cumulative_probability_of_task_success = follow_on_task.probability_of_task_success*task.cumulative_probability_of_task_success
 
-        cumulative_probability_of_task_success = task.probability_of_task_success
-        cumulative_cost_and_duration = task.cost_duration_tuples_list
-    else:
-        #print(all_tasks)
-        cumulative, all_tasks  = sum_cost_and_duration(task, all_tasks)
-        cumulative_cost_and_duration = cumulative
-        cumulative_probability_of_task_success = 1
+        # do this recursively
+        all_tasks = cost_and_duration_sum(follow_on_task, all_tasks)
+
+    for this_child_task in task.child_tasks:
+        all_tasks = cost_and_duration_sum(this_child_task, all_tasks)
+
+    return all_tasks
+
+
+def add_task_to_graph(G, task, all_tasks):
+    """
+    """
+    print("[TRACE] add_task_to_graph: ",task.description)
 
     #task.print()
     G.add_node("instance_"+str(task.instance_ID),
@@ -118,25 +134,19 @@ def add_task(G, task, all_tasks):
                  "description: "+str(task.description)+"\l"+
                  "instance ID: "+str(task.instance_ID)+"\l"+
                  "p(success): "+str(task.probability_of_task_success)+"\l"+
-                 "cumulative p(success): "+str(cumulative_probability_of_task_success)+"\l"+
+                 "cumulative p(success): "+str(round(task.cumulative_probability_of_task_success,5))+"\l"+
                  "this task (cost,dur)="+str(task.cost_duration_tuples_list)+"\l"+
-                 "cumulative (cost,dur)= "+str(cumulative_cost_and_duration)+"\l"+
+                 "cumulative (cost,dur)= "+str(task.cumulative_cost_and_duration)+"\l"+
                  "output param="+str(task.output_parameters_dict)+"\l",
            shape="rect");
     return G
 
-def add_task_that_has_subtasks(G, task, list_of_subtasks, all_tasks):
+def add_task_to_graph_that_has_subtasks(G, task, list_of_subtasks, all_tasks):
     """
     """
-    print("[TRACE] add_task_that_has_subtasks: ",task.description)
-    # TODO; trace costs using all_tasks
-    if task.description=="start here":
-        cumulative_probability_of_task_success = task.probability_of_task_success
-        cumulative_cost_and_duration = task.cost_duration_tuples_list
-    else:
-        cumulative, all_tasks  = sum_cost_and_duration(task, all_tasks)
-        cumulative_cost_and_duration = cumulative
-        cumulative_probability_of_task_success = 1
+    print("[TRACE] add_task_to_graph_that_has_subtasks: ",task.description)
+
+    #all_tasks[(task.description,task.instance_ID)].child_tasks = list_of_subtasks
 
     B = G.add_subgraph(name="cluster_instance_"+str(task.instance_ID),
                        label="condition: "+str(task.condition)+"\l"+
@@ -148,10 +158,10 @@ def add_task_that_has_subtasks(G, task, list_of_subtasks, all_tasks):
                              #"cumulative (cost,dur)= "+str(cumulative_cost_and_duration)+"\l"+
                              "output param="+str(task.output_parameters_dict)+"\l")
 
-    B.add_node("instance_"+str(task.instance_ID), style="invis");
+#    B.add_node("instance_"+str(task.instance_ID), style="invis");
 
     for subtask in list_of_subtasks:
-        B = add_task(B, subtask, all_tasks)
+        B = add_task_to_graph(B, subtask, all_tasks)
 
     return G
 
@@ -184,6 +194,31 @@ def add_edges(G, task):
 #
 #     return all_tasks
 
+def create_graph(G, all_tasks):
+    """
+    """
+    print("[TRACE] create_graph")
+    for desc, task in all_tasks.items():
+        # all_tasks = cumulative_probability_of_task_success(task, all_tasks)
+        #
+        # #print("last loop:",all_tasks)
+        # cumulative, all_tasks = sum_cost_and_duration(task, all_tasks)
+        #
+
+        #task.print()
+
+        #print("STATUS: now adding task to graph")
+        if len(task.child_tasks)==0:
+            G = add_task_to_graph(G, task, all_tasks)
+        else:
+            G = add_task_to_graph_that_has_subtasks(G, task,task.child_tasks,all_tasks)
+
+        #print("STATUS: now adding edges to graph")
+        G = add_edges(G, task)
+
+
+    return G
+
 if __name__ == "__main__":
     G = pygraphviz.AGraph(directed=True, compound=True)
     #G.add_node("start_here", label="start here")
@@ -191,21 +226,21 @@ if __name__ == "__main__":
     all_tasks = {}
 
     # need to designate an entry point for calculating cumulative sums and products
-    task = Task(6168,
-                 all_tasks,
+    task = Task(6168,all_tasks,
                  "always", # condition
                  "start here", # description
                  1, # probability of success of this task
                  [(0,0), (0,0)], # cost and duration
                  {}, # outputs
                  []) # followed by task IDs
+    task.cumulative_cost_and_duration = [(0,0), (0,0)]
+    task.cumulative_probability_of_task_success = 1
     all_tasks[(task.description, task.instance_ID)] = task
 
-    task = Task(5842,
-                all_tasks,
+    task = Task(5842,all_tasks,
                  "if f<=4:", # condition
                  "a task", # description
-                 0.99, # probability of success of this task
+                 0.91, # probability of success of this task
                  [(1,2), (3,4)], # cost and duration
                  {'a'}, # outputs
                  []) # followed by task IDs
@@ -213,11 +248,10 @@ if __name__ == "__main__":
 
 #    G.add_edge("start_here", "instance_"+str(task.instance_ID))
 
-    task = Task(6643,
-                 all_tasks,
+    task = Task(6643,all_tasks,
                  "always", # condition
                  "some task", # description
-                 0.99, # probability of success of this task
+                 0.9, # probability of success of this task
                  [(2,2), (3,4)], # cost and duration
                  {'f'}, # outputs
                  []) # followed by task IDs
@@ -235,14 +269,9 @@ if __name__ == "__main__":
                  []) # followed by task IDs
     all_tasks[(task.description, task.instance_ID)] = task
 
-    task = Task(2345,all_tasks,
-                 "always", # condition
-                 "the task", # description
-                 0.99, # probability of success of this task
-                 [(3,1), (3,4)], # cost and duration
-                 {'a'}, # outputs
-                 []) # followed by task IDs
-    all_tasks[(task.description, task.instance_ID)] = task
+    task_copy = copy.deepcopy(task)
+    task_copy.instance_ID = 2345
+    all_tasks[(task_copy.description, task_copy.instance_ID)] = task_copy
 
     task = Task(3456,all_tasks,
                  "always", # condition
@@ -253,26 +282,17 @@ if __name__ == "__main__":
                  []) # followed by task IDs
     all_tasks[(task.description, task.instance_ID)] = task
 
-    task = Task(1456,all_tasks,
-                 "always", # condition
-                 "last task", # description
-                 0.99, # probability of success of this task
-                 [(1,1), (1,1)], # cost and duration
-                 {'k'}, # outputs
-                 []) # followed by task IDs
-    all_tasks[(task.description, task.instance_ID)] = task
+    task_copy = copy.deepcopy(task)
+    task_copy.instance_ID = 1456
+    all_tasks[(task_copy.description, task_copy.instance_ID)] = task_copy
 
-    task = Task(2456,all_tasks,
-                 "always", # condition
-                 "last task", # description
-                 0.99, # probability of success of this task
-                 [(1,1), (1,1)], # cost and duration
-                 {'k'}, # outputs
-                 []) # followed by task IDs
-    all_tasks[(task.description, task.instance_ID)] = task
+    task_copy = copy.deepcopy(task)
+    task_copy.instance_ID = 2456
+    all_tasks[(task_copy.description, task_copy.instance_ID)] = task_copy
 
     all_tasks[("the task", 2345)].followed_by_task_instance_IDs.append(all_tasks[("last task",3456)].instance_ID)
     all_tasks[("the task", 1234)].followed_by_task_instance_IDs.append(all_tasks[("last task",1456)].instance_ID)
+
 
     task = Task(4567,all_tasks,
                  "always", # condition
@@ -298,7 +318,7 @@ if __name__ == "__main__":
     task = Task(6789,all_tasks,
                  "always", # condition
                  "la child task", # description
-                 0.99, # probability of success of this task
+                 0.8, # probability of success of this task
                  [(8,3), (3,4)], # cost and duration
                  {'c'}, # outputs
                  []) # followed by task IDs
@@ -307,7 +327,7 @@ if __name__ == "__main__":
     task = Task(7890,all_tasks,
                  "if c>=5", # condition
                  "el child task", # description
-                 0.99, # probability of success of this task
+                 0.4, # probability of success of this task
                  [(1,1), (2,4)], # cost and duration
                  {'b'}, # outputs
                  []) # followed by task IDs
@@ -322,35 +342,23 @@ if __name__ == "__main__":
                  []) # followed by task IDs
     all_tasks[(task.description, task.instance_ID)] = task
 
+    all_tasks[("parent task",5678)].child_tasks = [
+    all_tasks[("la child task", 6789)],
+    all_tasks[("el child task", 7890)],
+    all_tasks[("child task", 1357)]
+    ]
 
     all_tasks[("la child task", 6789)].followed_by_task_instance_IDs.append(all_tasks[("el child task", 7890)].instance_ID)
     all_tasks[("la child task", 6789)].followed_by_task_instance_IDs.append(all_tasks[("child task", 1357)].instance_ID)
-    all_tasks[("some task", 6643)].followed_by_task_instance_IDs.append(all_tasks[("parent task", 5678)].instance_ID)
-
-    G = add_task_that_has_subtasks(G, all_tasks[("parent task", 5678)],
-                                   [all_tasks[("el child task",7890)],
-                                    all_tasks[("child task", 1357)],
-                                    all_tasks[("la child task", 6789)]
-                                   ],
-                                   all_tasks)
+    all_tasks[("some task", 6643)].followed_by_task_instance_IDs.append(all_tasks[("la child task", 6789)].instance_ID)
 
     all_tasks[("child task", 1357)].followed_by_task_instance_IDs.append(all_tasks[("the task", 1234)].instance_ID)
     all_tasks[("el child task", 7890)].followed_by_task_instance_IDs.append(all_tasks[("the task", 2345)].instance_ID)
 
-    for desc, task in all_tasks.items():
-        # all_tasks = cumulative_probability_of_task_success(task, all_tasks)
-        #
-        # #print("last loop:",all_tasks)
-        # cumulative, all_tasks = sum_cost_and_duration(task, all_tasks)
-        #
+    all_tasks = cost_and_duration_sum(all_tasks["start here",6168], all_tasks)
 
-        task.print()
+    print("all_tasks",all_tasks)
 
-        print("STATUS: now adding task to graph")
-        G = add_task(G, task, all_tasks)
-
-        print("STATUS: now adding edges to graph")
-        G = add_edges(G, task)
-
-
+    G = create_graph(G, all_tasks)
+    #print(G)
     G.draw("automated_digraph.png", prog="dot")
